a21cfa6bfac13f083fdd806dc2c06b48c3076955
municipiodejaguare <73909177+municipiodejaguare@users.noreply.github.com>
2022-07-13 08:15:01 -0300

    fim

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,32 @@
 import NotificationButton from "./components/NotificationButton"
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 function App() {
+  const min = new Date(new Date().setDate(new Date().getDate() - 365));
+  const max = new Date();
+
+  const [minDate, setMinDate] = useState(min);
+  const [maxDate, setMaxDate] = useState(max);
+
 
   return (
     <>
       <h1>Olá mundo!</h1>
       <NotificationButton></NotificationButton>
+      <DatePicker
+        selected={minDate}
+        onChange={(date: Date) => setMinDate(date)}
+        className="dsmeta-form-control"
+        dateFormat="dd/MM/yyyy"
+      />
+      <DatePicker
+        selected={maxDate}
+        onChange={(date: Date) => setMaxDate(date)}
+        className="dsmeta-form-control"
+        dateFormat="dd/MM/yyyy"
+      />
     </>
   )
 }
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import icon from '../../assets/img/notification-icon.svg'
+
+function Header(){
+    return (
+       <div className={`dsmeta-red-btn`}>
+        <img src={icon} alt="logo" />
+       </div>
+    )
+}
+
+export default Header;
